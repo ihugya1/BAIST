20593229bba92ae8fb68fab7f1ba30d6c60f2d92
Ian <50344364+ihugya1@users.noreply.github.com>
2020-11-05 15:34:32 -0700

    Added fields to the collection

--- BAIST3150RazorPagesNETCore31/Pages/DynamicFormDisplaySample.cshtml.cs
+++ BAIST3150RazorPagesNETCore31/Pages/DynamicFormDisplaySample.cshtml.cs
@@ -4,13 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BAIST3150RazorPagesNETCore31.Domain;
 
 namespace BAIST3150RazorPagesNETCore31.Pages
 {
     public class DynamicFormDisplaySampleModel : PageModel
     {
+        public string Message { get; set; }
+        [BindProperty]
+        public string AField { get; set; }
+        [BindProperty]
+        public string ACollection { get; set; }
+        public List<SampleClass> SampleObjectCollection { get; } = new List<SampleClass>();
         public void OnGet()
         {
+            Message = "*** On Get ***";
+
+            SampleClass SampleObject;
+            SampleObject = new SampleClass();
+            SampleObject.FirstProperty = "1";
+            SampleObject.SecondProperty = "One";
+            SampleObjectCollection.Add(SampleObject);
+
+            SampleObject = new SampleClass
+            {
+                FirstProperty = "2",
+                SecondProperty = "Two"
+            };
+            SampleObjectCollection.Add(SampleObject);
+            SampleObject = new SampleClass
+            {
+                FirstProperty = "3",
+                SecondProperty = "Three"
+            };
+            SampleObjectCollection.Add(SampleObject);
         }
     }
 }
