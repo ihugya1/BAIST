8120989507d366305717fa505afb1c6acaa66389
Ian <50344364+ihugya1@users.noreply.github.com>
2020-11-05 14:29:36 -0700

    Added some comments

--- BAIST3150RazorPagesNETCore31/Pages/ValidationDAA.cshtml.cs
+++ BAIST3150RazorPagesNETCore31/Pages/ValidationDAA.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,8 +10,27 @@ namespace BAIST3150RazorPagesNETCore31.Pages
 {
     public class ValidationDAAModel : PageModel
     {
+        
+        public string Message { get; set; }
+        [BindProperty]
+        [Required]
+        [StringLength(5,MinimumLength = 1)]
+        public string InputField { get; set; }
         public void OnGet()
         {
+            Message = " on Get ";
+        }
+        public void OnPost()
+        {
+            
+            if (ModelState.IsValid)
+            {
+                Message = " On Post is Valid";
+            }
+            else
+            {
+                Message = " On Post is NOT Valid";
+            }
         }
     }
 }
