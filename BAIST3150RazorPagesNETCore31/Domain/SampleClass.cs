7828bd4d55c6203204ee53560f6b2c21caffce67
Ian <50344364+ihugya1@users.noreply.github.com>
2020-11-05 15:26:43 -0700

    Added Dynammic Form Display Sample Page

---
+++ BAIST3150RazorPagesNETCore31/Domain/SampleClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BAIST3150RazorPagesNETCore31.Domain
+{
+    public class SampleClass
+    {
+        public string FirstProperty { get; set; }
+        public string SecondProperty { get; set; }
+    }
+}
--- BAIST3150RazorPagesNETCore31/Pages/DynamicDisplaySample.cshtml.cs
+++ BAIST3150RazorPagesNETCore31/Pages/DynamicDisplaySample.cshtml.cs
@@ -2,15 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BAIST3150RazorPagesNETCore31.Domain;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BAIST3150RazorPagesNETCore31.Pages
 {
+ 
+
     public class DynamicDisplaySampleModel : PageModel
     {
+        private List<SampleClass> _sampleObjectCollection = new List<SampleClass>();
+        public List<SampleClass> SampleObjectCollection
+        {
+            get
+            {
+                return _sampleObjectCollection;
+            }
+        }
         public void OnGet()
         {
+            SampleClass SampleObject;
+            SampleObject = new SampleClass();
+            SampleObject.FirstProperty = "1";
+            SampleObject.SecondProperty = "One";
+            SampleObjectCollection.Add(SampleObject);
+
+            SampleObject = new SampleClass
+            {
+                FirstProperty = "2",
+                SecondProperty = "Two"
+            };
+            SampleObjectCollection.Add(SampleObject);
+            SampleObject = new SampleClass
+            {
+                FirstProperty = "3",
+                SecondProperty = "Three"
+            };
+            SampleObjectCollection.Add(SampleObject);
+
         }
     }
 }
