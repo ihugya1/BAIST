1e74e3e2fab846a21cfc2abb5eba1b3f06c395f3
Ian <50344364+ihugya1@users.noreply.github.com>
2020-11-12 12:46:39 -0700

    got rid of stuff

--- BCS-UI-Test/BLL/CodeHandler.cs
+++ BCS-UI-Test/BLL/CodeHandler.cs
@@ -12,19 +12,11 @@ namespace BAIS3150_OOPAssignment01_IanHugya_OA02.Domain
         //Conroller = Call the Manager -> Return what was returned
         public bool AddCourse(Course aNewCourse)
         {
-            
+            Console.WriteLine("HIT");
             bool confirmation;
             Courses courseManager = new Courses();
             confirmation = courseManager.AddCourse(aNewCourse);
             return confirmation;
-        }
-        public List<Product> GetProductsForCategory(int categoryID)
-        {
-            List<Product> productList = new List<Product>();
-            Categories categoryManager = new Categories();
-            productList = categoryManager.GetProductsByCategories(categoryID);
-            return productList;
-
         }
     }
 }
